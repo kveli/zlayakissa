9b1ec1b3ecaf7ae05e53b3a2e4228757e2bf3603
kveli <38850886+kveli@users.noreply.github.com>
2020-07-21 21:42:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
-#token = os.environ.get('token-telegram')
-token = '1330647759:AAFzeP1ssOiMShqtGx1E8NohvwbvokPHFLc'
-channel_name = '@zlaya12356'
+token = os.environ.get('token-telegram')
+channel_name = '@zlayakissapwm'
